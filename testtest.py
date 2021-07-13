049c9c81d0045e43543f3cdc560d8dc6e794489d
Sean OToole <seanotoole@u.boisestate.edu>
2021-07-13 12:13:28 -0600

    Update testtest.py pyauto
    added pyautogui run test

--- testtest.py
+++ testtest.py
@@ -5,3 +5,9 @@ def test_add():
   assert add(2,3) == 5
   assert add('space', 'ship') == 'spaceship'
   
+# pyautogui tests
+import pyautogui
+
+screenWidth,screenHeight = pyautogui.size()
+currentMouseX, currentMouseY = pyautogui.position()
+pyautogui.moveto(screenWidth/2, screenHeight/2)
